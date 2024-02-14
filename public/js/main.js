01d9646a4111007ead0e217961568543bd5108f9
glinary <glarclone@gmail.com>
2024-02-14 10:56:22 +0800

    removed Testing1

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-function connect(x) {
-    return x;
-}
-
-module.exports = {connect};
-
-
-// IMPORTANT: Do not use "test" as the name of the function
--- public/testing/main.test.js
+++
@@ -1,6 +0,0 @@
-const { connect } = require('../js/main');
-
-
-test('if return 0, test passes', () => {
-    expect(connect(0)).toBe(0);
-});
