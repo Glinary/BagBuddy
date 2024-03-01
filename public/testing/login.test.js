7a04d54e4a70cd6a3edeea7d60296642e115b5e3
Sarah Jumilla <sarahericka.jumilla@gmail.com>
2024-03-01 20:47:29 +0800

    Update testing login function

--- public/testing/login.test.js
+++ public/testing/login.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../../index.js');
 const { login }  = require('../js/login');
+const Users = require('../../schema/Users');
 
 /*
     This function tests to see if a registered set of email, and password
@@ -45,4 +46,45 @@ describe("POST /login", () => {
 
 test('unsuccessful login does not return 200', () => {
     expect(login("first_last@dlsu.edu.ph", "P@ssw0rd")).not.toBe(200);
+});
+
+/****************************************************************************************/
+jest.mock('../../schema/Users', () => ({
+    findOne: jest.fn(),
+}));
+
+describe('Login Function', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('returns 200 for successful login', async() => {
+        const mockUser = {compare: jest.fn(() => true)};
+        Users.findOne.mockResolvedValue(mockUser);
+
+        const statusCode = await login('gleezell_uy@dlsu.edu.ph', 'P@ssw0rd');
+        expect(statusCode).toBe(200);
+
+        expect(Users.findOne).toHaveBeenCalledWith({email: 'gleezell_uy@dlsu.edu.ph' });
+        expect(mockUser.compare).toHaveBeenCalledWith('P@ssw0rd');
+        });
+
+    test('returns 402 given that password is incorrect', async() => {
+        const mockUser = {compare: jest.fn(() => false)};
+        Users.findOne.mockResolvedValue(mockUser);
+
+        const statusCode =await login('gleezell_uy@dlsu.edu.ph','wrongP@ssw0rd');
+        expect(statusCode).toBe(402);
+
+        expect(Users.findOne).toHaveBeenCalledWith({ email:'gleezell_uy@dlsu.edu.ph' });
+        expect(mockUser.compare).toHaveBeenCalledWith('wrongP@ssw0rd');
+    });
+    test('returns 403 given that email does not exist', async() => {
+        Users.findOne.mockResolvedValue(null);
+
+        const statusCode = await login('hello@dlsu.edu.ph', 'MyP@ssw0rd');
+        expect(statusCode).toBe(403);
+        
+        expect(Users.findOne).toHaveBeenCalledWith({ email:'hello@dlsu.edu.ph' });
+    });
 });
