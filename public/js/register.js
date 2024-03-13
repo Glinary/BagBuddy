8137ca9312565f3fc42d1060ef59f36ca5c18ee1
ImAmeliaJoyce <amelia_abenoja@dlsu.edu.ph>
2024-03-13 10:19:58 +0800

    Fixed register validation

--- public/js/register.js
+++ public/js/register.js
@@ -1,5 +1,3 @@
-// const Users = require('../../schema/Users');
-
 var nameInput       = document.getElementById('register-name');
 var emailInput      = document.getElementById('register-email');
 var passwordInput   = document.getElementById('register-password');
@@ -20,14 +18,13 @@ function validateName() {
     console.log(isValidName);
 
     if (!isValidName) {
-        nameFeedback.textContent = 'Please enter a valid name. It must be characters only.';
+        nameFeedback.textContent = 'Please enter a valid name. It must be letters only.';
         nameFeedback.style.color = 'var(--main-red)';
         nameFeedback.style.display = 'block';
     } else {
         if (nameLength >= 7 && nameLength <= 36) {
-            nameFeedback.textContent = 'Valid Name';
-            nameFeedback.style.color = 'green';
-            nameFeedback.style.display = 'block';
+            // hide feedback
+            nameFeedback.style.display = 'none';
         } else {
             nameFeedback.textContent = 'Name must be 7 to 36 characters.';
             nameFeedback.style.color = 'var(--main-red)';
@@ -50,9 +47,8 @@ function validateEmail() {
         emailFeedback.style.color = 'var(--main-red)';
         emailFeedback.style.display = 'block';
     } else {
-        emailFeedback.textContent = 'Valid email';
-        emailFeedback.style.color = 'green';
-        emailFeedback.style.display = 'block';
+        // hide feedback
+        emailFeedback.style.display = 'none';
     }
 }
 
@@ -60,13 +56,12 @@ function validatePassword() {
     let passwordlength = passwordInput.value.length;
 
     if (passwordlength < 7) {
-        passwordFeedback.textContent = 'Invalid length pf password. Must be at least 7 characters.';
+        passwordFeedback.textContent = 'Invalid length of password. Must be at least 7 characters.';
         passwordFeedback.style.color = 'var(--main-red)';
         passwordFeedback.style.display = 'block';
     } else {
-        passwordFeedback.textContent = 'Valid password';
-        passwordFeedback.style.color = 'green';
-        passwordFeedback.style.display = 'block';
+        // hide feedback
+        passwordFeedback.style.display = 'none';
     }
 }
 
@@ -75,37 +70,97 @@ nameInput.addEventListener('keyup', validateName);
 emailInput.addEventListener('keyup', validateEmail);
 passwordInput.addEventListener('keyup', validatePassword);
 
-document.getElementById('registration-form').addEventListener('submit', function(event) {
-    if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
-        event.preventDefault();
 
-        if (nameInput.value.trim() === '') {
-            nameFeedback.textContent = 'All fields are required.';
-            nameFeedback.style.color = 'var(--main-red)';
-            nameFeedback.style.display = 'block';
-        }
+document.getElementById('register-submit-btn').addEventListener('click', async function(event) {
+    event.preventDefault();
+    
+    const formElement = document.getElementById('registration-form');
+    const formData = new FormData(formElement);
+    
+    // Convert FormData to JSON for sending to the server
+    const data = Object.fromEntries(formData.entries());
+    console.log('Form data:', data);
+    
+    // Name validation
+    const nameResponse = await fetch(`/register/isNameValid`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ registerName: nameInput.value }),
+    });
 
-        if (emailInput.value.trim() === '') {
-            emailFeedbackFeedback.textContent = 'All fields are required.';
-            emailFeedback.style.color = 'var(--main-red)';
-            emailFeedback.style.display = 'block';
-        }
+    const nameData = await nameResponse.json();
+    console.log(nameData);
 
-        if (passwordInput.value.trim() === '') {
-            passwordFeedback.textContent = 'All fields are required.';
-            passwordFeedback.style.color = 'var(--main-red)';
-            passwordFeedback.style.display = 'block';
+    if (nameData.message === 'Name already in use.') {
+        nameFeedback.textContent = 'Name already in use.';
+        nameFeedback.style.color = 'var(--main-red)';
+        nameFeedback.style.display = 'block';
+    }
+
+    // Email validation
+    const emailResponse = await fetch(`/register/isEmailValid`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ registerEmail: emailInput.value }),
+    });
+
+    const emailData = await emailResponse.json();
+    console.log(emailData);
+
+    if (emailData.message === 'Email already in use.') {
+        emailFeedback.textContent = 'Email already in use.';
+        emailFeedback.style.color = 'var(--main-red)';
+        emailFeedback.style.display = 'block';
+    }
+
+    if (nameData.message === 'Name is valid.' && emailData.message === 'Email is valid.') {
+        try {
+            // Send the form data to the server
+            const response = await fetch('/register/newUserRegistration', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            
+            // Check if the response is ok
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            
+            // Parse the JSON response
+            const responseData = await response.json();
+            console.log(responseData);
+
+            // If the response is successful, redirect to the login validation
+            // if (responseData.message === 'User successfully registered.') {
+            //     const loginResponse = await fetch('/login', {
+            //         method: 'POST',
+            //         headers: {
+            //             'Content-Type': 'application/json',
+            //         },
+            //         body: JSON.stringify({ email: emailInput.value, password: passwordInput.value }),
+            //     });
+
+            //     // Check if the response is ok
+            //     if (!loginResponse.ok) {
+            //         throw new Error('Network response was not ok');
+            //     }
+
+            //     // Parse the JSON response
+            //     const loginData = await loginResponse.json();
+            //     console.log(loginData);
+            // }
+
+        } catch (error) {
+            console.error('Error registering user:', error);
         }
+    } else {
+        console.log('Form data is invalid');
     }
 });
-
-
-function register(name, email, password) {
-    let resultName = validateName();
-    let resultEmail = validateEmail();
-    let resultPassword = validatePassword();
-
-
-}
-
-module.exports  = { register };
