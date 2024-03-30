d9c3b7efdae471707346b75cfb68335894384be7
glinary <glarclone@gmail.com>
2024-03-31 02:29:56 +0800

    Testing4 - added GET /home test

--- public/testing/controller.test.js
+++ public/testing/controller.test.js
@@ -14,3 +14,20 @@ describe("GET /", () => {
         })
     })
 })
+
+describe("GET /home", () => {
+    describe("when home page loads with user id", () => {
+        test("return a status code 200", async () => {
+            async (req) => {
+                await supertest(app).get(`/home/${req.session.user}`).expect(200);
+            };
+            
+        })
+    })
+
+    describe("when home page loads without user id", () => {
+        test("return a status code 404", async () => {
+            await supertest(app).get("/home").expect(404);
+        })
+    })
+})
